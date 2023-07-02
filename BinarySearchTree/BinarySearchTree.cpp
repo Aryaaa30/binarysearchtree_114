e114aab9d02feca984ac68d772fab4dc3e978e1f
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-07-02 18:14:49 +0700

    Apabila ptr nya tidak sama dengan NULL

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -102,6 +102,12 @@ public:
 			cout << "Tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
 	}
 };
 
