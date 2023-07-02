0d96efb512f419d058ecb08c9cb49e53244bde66
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-07-02 17:52:04 +0700

    Membuat prosedur if else nya

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -60,6 +60,10 @@ public:
 		while ((currentNode != NULL) && (currentNode->info != element))
 		{
 			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
 		}
 	}
 };
